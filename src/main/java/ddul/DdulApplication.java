b972543db52ff5ef910e55ee97267c90e2de7a9b
green <greenn.lab@gmail.com>
2021-06-24 21:50:10 +0900

    Initial commit

---
+++ src/main/java/ddul/DdulApplication.java
@@ -0,0 +1,13 @@
+package ddul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DdulApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ddul/DdulApplicationTests.java
@@ -0,0 +1,13 @@
+package ddul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdulApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
