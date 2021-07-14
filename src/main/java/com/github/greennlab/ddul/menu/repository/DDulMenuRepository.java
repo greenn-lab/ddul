a6e9e65cba92c5b79be50878674debf388b6deb1
green <greenn.lab@gmail.com>
2021-07-14 10:15:12 +0900

    update// change extend

--- src/main/java/com/github/greennlab/ddul/menu/repository/DDulMenuRepository.java
+++ src/main/java/com/github/greennlab/ddul/menu/repository/DDulMenuRepository.java
@@ -2,7 +2,10 @@ package com.github.greennlab.ddul.menu.repository;
 
 import com.github.greennlab.ddul.entity.AFewRepository;
 import com.github.greennlab.ddul.menu.Menu;
+import java.util.List;
 
 public interface DDulMenuRepository extends AFewRepository<Menu, Long> {
+
+  <S extends Menu> List<S> saveAll(Iterable<S> var1);
 
 }
