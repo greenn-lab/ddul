109269a6da0aafdab929f78a1b8b6c1fd6ecaf95
green <greenn.lab@gmail.com>
2021-07-15 17:38:34 +0900

    update// code save

--- src/main/java/com/github/greennlab/ddul/code/web/DDulCodeController.java
+++ src/main/java/com/github/greennlab/ddul/code/web/DDulCodeController.java
@@ -1,12 +1,17 @@
 package com.github.greennlab.ddul.code.web;
 
+import static com.github.greennlab.ddul.code.CommonCode.mapped;
+
 import com.github.greennlab.ddul.code.CommonCode;
 import com.github.greennlab.ddul.code.repository.DDulCodeRepository;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +26,21 @@ public class DDulCodeController {
   @GetMapping
   public List<CommonCode.Dto> getGroupCodes(String group, String keyword) {
     return repository.findAllByGroupAndNameContainsOrderByOrder(group, keyword).stream()
-        .map(CommonCode.mapped::to)
+        .map(mapped::to)
         .collect(Collectors.toList());
   }
 
   @GetMapping("/{group}")
   public List<CommonCode.Dto> getCodes(@PathVariable String group) {
     return repository.findAllByGroupOrderByOrder(group).stream()
-        .map(CommonCode.mapped::to)
+        .map(mapped::to)
         .collect(Collectors.toList());
   }
 
+  @PostMapping
+  public CommonCode.Dto save(@Valid @RequestBody CommonCode.Dto dto) {
+    final CommonCode code = repository.save(mapped.by(dto));
+    return mapped.to(code);
+  }
 
 }
