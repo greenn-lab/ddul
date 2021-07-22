ec4cbe3a75859b4f5d76ccf4ed4bf0d4a17aa57c
green <greenn.lab@gmail.com>
2021-07-22 18:01:57 +0900

    update// change password encoder

--- src/main/java/com/github/greennlab/ddul/DDulSecurityConfiguration.java
+++ src/main/java/com/github/greennlab/ddul/DDulSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.github.greennlab.ddul;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ConditionalOnMissingBean(AuthenticationConfiguration.class)
@@ -25,6 +28,11 @@ public class DDulSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .antMatchers("/*").permitAll()
     ;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 
 }
