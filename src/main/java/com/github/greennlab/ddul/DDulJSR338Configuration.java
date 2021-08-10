d4adef06ab825f60514427fa7e2a2f68c4e8603d
green <greenn.lab@gmail.com>
2021-08-10 09:25:15 +0900

    update// repositioning auditor bean

--- src/main/java/com/github/greennlab/ddul/DDulJSR338Configuration.java
+++ src/main/java/com/github/greennlab/ddul/DDulJSR338Configuration.java
@@ -1,10 +1,33 @@
 package com.github.greennlab.ddul;
 
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableJpaAuditing
 public class DDulJSR338Configuration {
+
+  @Bean
+  AuditorAware<String> securityLinkageAuditorAware() {
+    return () -> {
+      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication == null) {
+        return Optional.of("{ghost}");
+      }
+
+      final Object principal = authentication.getPrincipal();
+      if (principal instanceof User) {
+        return Optional.of(((User) principal).getUsername());
+      }
+
+      return Optional.of(principal.toString());
+    };
+  }
 
 }
--- src/main/java/com/github/greennlab/ddul/DDulWebAppConfiguration.java
+++ src/main/java/com/github/greennlab/ddul/DDulWebAppConfiguration.java
@@ -10,35 +10,13 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.Locale;
-import java.util.Optional;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class DDulWebAppConfiguration {
-
-  @Bean
-  AuditorAware<String> securityLinkageAuditorAware() {
-    return () -> {
-      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      if (authentication == null) {
-        return Optional.of("{ghost}");
-      }
-
-      final Object principal = authentication.getPrincipal();
-      if (principal instanceof User) {
-        return Optional.of(((User) principal).getUsername());
-      }
-
-      return Optional.of(principal.toString());
-    };
-  }
 
   @Bean
   SessionLocaleResolver sessionLocaleResolver() {
