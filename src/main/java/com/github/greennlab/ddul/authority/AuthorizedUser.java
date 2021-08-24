1f8fa0fb14a3023301de0c6fbb6cf6877cbdab86
pinethreee <pinethreee@gmail.com>
2021-08-25 01:06:35 +0900

    update// easy instantiation AuthorizedUser

--- src/main/java/com/github/greennlab/ddul/authority/AuthorizedUser.java
+++ src/main/java/com/github/greennlab/ddul/authority/AuthorizedUser.java
@@ -2,13 +2,17 @@ package com.github.greennlab.ddul.authority;
 
 import com.github.greennlab.ddul.user.User;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -23,6 +27,13 @@ public class AuthorizedUser implements UserDetails {
 
   private final Set<? extends GrantedAuthority> authorities;
 
+  public AuthorizedUser(String username, String... authorities) {
+    this.user = new User();
+    this.user.setUsername(username);
+
+    this.authorities = Arrays.stream(authorities).map(
+        SimpleGrantedAuthority::new).collect(Collectors.toSet());
+  }
 
   public static Optional<User> currently() {
     final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
